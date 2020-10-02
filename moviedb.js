8a69893ab1099ff85322701c430d3248c998a560
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-10-02 17:05:39 +0200

    code cleaned

--- moviedb.js
+++ moviedb.js
@@ -23,33 +23,4 @@ program
 
 program.parse(process.argv)
 
-//if(program.test) console.log("The program is running")
-function apiCall22(page) {
-    //https://nodejs.org/api/https.html#https_https_request_options_callback
-    const options = {
-        host: 'api.themoviedb.org',
-        port: 443,
-        path: `/3/person/popular?page=${page}&api_key=${process.env.API_KEY}`,
-        method: 'GET'
-    };
-    //START REQUEST
-    https.request( options, (res) => {
-        let response = "";
-        res.on('data', (d) => {
-            //process.stdout.write(d);
-            response += d
-        });
-        res.on('end', (d) => {
-            let json = JSON.parse(response)
-            console.log(json)
-            return json;
-        });
-    })
-    .on('error', (e) => {
-        console.error(e);
-        return e;
-    })
-    .end();
-    //END REQUEST
-}
 
