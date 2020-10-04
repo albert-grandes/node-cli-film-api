d29de841303c9b252805ae1f4971cb01e14f90c0
Yulia Belyakova <yuliya.beliakova@gmail.com>
2020-10-04 17:14:59 +0200

    fetchin data by id added

--- moviedb.js
+++ moviedb.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {apiCallPopular} from './personsFunctions.js'
+import {fetchPopular, fetchById} from './persons.js'
 import program from 'commander'
 import dotenv from 'dotenv'
 
@@ -10,10 +10,18 @@ dotenv.config()
 program
   .command('get-person')
   .description('Make a network request to fetch the most popular persons')
+  .requiredOption('--id <number>', 'get person')
+  .action((options) => {
+    fetchById(options.id)
+  })
+
+  program
+  .command('get-persons')
+  .description('Make a network request to fetch person by id')
   .requiredOption('-p --popular', 'Fetch the popular persons')
   .requiredOption('--page <number>', 'get popular persons')
   .action((options) => {
-    apiCallPopular(options.page)
+    fetchPopular(options.page)
   })
 
 program.parse(process.argv)
--- persons.js
+++ persons.js
@@ -3,7 +3,7 @@ import ora from 'ora'
 import chalk from 'chalk'
 
 
-export function apiCallPopular(page) {
+export function fetchPopular(page) {
     //https://nodejs.org/api/https.html#https_https_request_options_callback
    
     const options = {
@@ -12,31 +12,45 @@ export function apiCallPopular(page) {
         path: `/3/person/popular?page=${page}&api_key=${process.env.API_KEY}`,
         method: 'GET'
     };
-    //START REQUEST
     const spinner = ora('Loading popular').start()
     https.request( options, (res) => {
-      
         let response = "";
         res.on('data', (d) => {
-            //process.stdout.write(d);
             response += d 
         });
         res.on('end', (d) => { 
             let json = JSON.parse(response)
             printPopular(json)
-            spinner.succeed("Popular loaded");
-                        
+            spinner.succeed("Popular loaded");                   
         });
-
     })
     .on('error', (e) => {
       spinner.fail(`Something went wrong! Error message: ${e.message}`)
     })
     .end();
-    //END REQUEST
 }
 
-//export default apiCall
+export function fetchById(id) {
+    //https://nodejs.org/api/https.html#https_https_request_options_callback
+   
+    const options = {
+        host: 'api.themoviedb.org',
+        port: 443,
+        path: `/3/person/${id}?api_key=${process.env.API_KEY}`,
+        method: 'GET'
+    };
+    const spinner = ora('Fetchin person data').start()
+    https.request( options, (res) => {
+        res.on('data', (data) => { 
+            printById(JSON.parse(data))
+            spinner.succeed("Person loaded");                   
+        });
+    })
+    .on('error', (e) => {
+      spinner.fail(`Something went wrong! Error message: ${e.message}`)
+    })
+    .end();
+}
 
 function printPopular(popular) {
     console.log('\n')
@@ -58,8 +72,28 @@ function printPopular(popular) {
                 console.log(chalk.white(`\t Title: ${movie.title} \n`))
             }
         }  else {
-            console.log(chalk.white(`\t ${person.name} does not appear in any movies`))
+            console.log(chalk.yellow.bold(`\t ${person.name} does not appear in any movies`))
         }
         console.log('\n')       
+    }
+}
+
+function printById(person) {
+    console.log('\n')
+    console.log(chalk.white('------------------'))
+    
+    console.log(chalk.white('Person:'))
+    console.log(chalk.white(`ID: ${person.id}`))
+    console.log(chalk.white('Name: ') + chalk.blue.bold(person.name))
+    console.log(chalk.white(`Birthday: ${person.birthday}`) + chalk.grey(' | ') + chalk.white(person.place_of_birth))
+    if(person.known_for_department) console.log(chalk.white('Deaprtment: ') + chalk.magenta.bold(person.known_for_department)) 
+    console.log(chalk.white('Biogarphy: ') + chalk.blue.bold(person.biography))
+    console.log(chalk.white('\nAlso known as:'))
+    if(person.also_known_as.length > 0) {
+        for(let name of person.also_known_as) {
+            console.log(chalk.white(name))
+        } 
+    } else {
+        console.log(chalk.yellow.bold(`${person.name} doesn't have other names`))
     }
 }
